fbcb8416c980d8984475e76047b574f2c9e42b3c
Ronitd2 <ronitd552@gmail.com>
2024-06-01 00:27:38 +0530

    github link added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,26 +18,28 @@ export default function Home()
             <div  className="mt-7 sm:mt-20  w-[80%] ">
                 <div className="relative">
                     <h1 className=" text-3xl sm:text-4xl font-semibold heading1 scale-up-tl">Hey there !,I'm-</h1>
-                    <h1 className=" text-6xl mt-5 sm:text-9xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient font-bold sm:ml-7 sm:mt-0 letter-by-letter-text">{text}</h1>
-
+                    <h1 className=" text-6xl mt-5 sm:text-9xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient font-bold  sm:mt-0 letter-by-letter-text">{text}</h1>
+                    
                 <div className="mt-8 sm:w-[75%] scale-up-right">
                     <p className=" text-2xl sm:text-4xl text-white font-semibold inline">Software Engineer.</p>
                     <p className="inline  text-2xl sm:text-4xl text2 font-semibold">A self-taught developer with an
                         interest in Computer Science.</p>
                     <div className="flex gap-4 mt-8">
                         <a href={resumepdf}>
-                        <div className="bg-[#3F4E4F] rounded-lg w-[35%] hover:bg-[#389f68] sm:w-[20%] md:w-auto p-2 text-center flex gap-2 items-center justify-center">
+                        <div className="bg-[#3F4E4F] rounded-lg  hover:bg-[#389f68]  w-auto p-2 text-center flex gap-2 items-center justify-center">
                         
                             <img className="w-5 h-6 " src={resume}></img>
-                           <p className="text-white font-semibold">Resume</p>
+                           <p className="text-white font-semibold">View CV</p>
                            
                         </div>
                         </a>
 
-                        <div className="bg-[#3F4E4F] rounded-lg hover:bg-[#389f68] w-[35%] sm:w-[20%] md:w-[15%] p-2 text-center  flex gap-2 items-center justify-center">
+                        <a href="https://github.com/Ronitd2">
+                        <div className="bg-[#3F4E4F] rounded-lg hover:bg-[#389f68]  w-auto p-2 text-center  flex gap-2 items-center justify-center">
                             <img className="w-6 h-6 " src={github}></img>
                            <p className="text-white font-semibold"> Github</p>
                         </div>
+                        </a>
                     </div> 
                 </div>
                 </div>
